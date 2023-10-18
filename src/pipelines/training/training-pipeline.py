8ae62b2e130d95f0bb7925499203badcbbf6ac34
jcheung <jacheung6@gmail.com>
2023-10-18 04:14:29 -0700

    add mlflow

--- src/pipelines/training/training-pipeline.py
+++ src/pipelines/training/training-pipeline.py
@@ -1,3 +1,4 @@
+import mlflow
 import tensorflow as tf
 import argparse
 # Project imports
@@ -5,10 +6,13 @@ from components.load_step import load
 from components.preprocess_step import preprocess
 from components.train_test_split_step import tfds_train_test_split
 from components.model_step import model
+from components.utils import set_mlflow_experiment
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--mlflow_experiment_name", type=str,
+                        help="MLflow experiment name")
     parser.add_argument("--epochs", type=int, default=10,
                          help="Number of epochs")
     parser.add_argument("--l1", type=float, default=0,
@@ -25,6 +29,11 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
+
+    # mlflow experiment
+    experiment_id = set_mlflow_experiment(args.mlflow_experiment_name)
+
+    # begin data preparation of pipeline
     data = load.mini_load_tensorflow_dataset(dataset_str='mnist',
                                             percent_of_dataset=10)
     data = data.map(preprocess.preprocess_mnist_tfds,
@@ -32,6 +41,7 @@ if __name__ == "__main__":
     train_dataset, test_dataset = tfds_train_test_split.train_test_split(data=data,
                                                                         train_proportion=.75,
                                                                         test_proportion=.25)
+    # instantiate hyperparameters from arguments. This'll be changed for katib
     hyperparameters = {'epochs': args.epochs,
                     'l1': args.l1,
                     'l2': args.l2, 
@@ -39,7 +49,20 @@ if __name__ == "__main__":
                     'learning_rate': args.learning_rate}
 
     MNIST = model.MNIST()
-    MNIST.fit_hp_search(xy_train=train_dataset,
-                        xy_test=test_dataset,
-                        hyperparameters=hyperparameters)
+    with mlflow.start_run():
+        MNIST.fit_hp_search(xy_train=train_dataset,
+                            xy_test=test_dataset,
+                            hyperparameters=hyperparameters)
+        # MLFlow Tracking parameters
+        mlflow.log_params(params=hyperparameters)
+
+        # MLFlow Tracking metrics 
+        # Logging metrics for each epoch (housed in dictionary)
+        training_history = MNIST._train_history.history
+        for epoch in range(0, hyperparameters['epochs']):
+            insert = {}
+            for metric, value in training_history.items():
+                insert[metric] = training_history[metric][epoch]
+            mlflow.log_metrics(metrics=insert, step=epoch+1)
+        
 
