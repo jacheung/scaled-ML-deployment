aa1ec3be7db702237e364b7252520e4ad625f703
jcheung <jacheung6@gmail.com>
2024-01-23 14:15:10 -0800

    Build preprocess component

--- src/components/preprocess_step/preprocess_component.py
+++ src/components/preprocess_step/preprocess_component.py
@@ -3,22 +3,22 @@ from kfp.dsl  import InputPath, OutputPath
 import tensorflow as tf
 import argparse
 
-def preprocess_component(data_file_input: InputPath(tf.data.Dataset), 
-                         pp_data_file_output: OutputPath(tf.data.Dataset)):
+def preprocess_component(dataset_file_input: InputPath(tf.data.Dataset), 
+                         pp_dataset_file_output: OutputPath(tf.data.Dataset)):
     # load data from previous step
-    data = tf.data.Dataset.load(data_file_input)
+    data = tf.data.Dataset.load(dataset_file_input)
 
     data = data.map(preprocess_mnist_tfds, num_parallel_calls=tf.data.AUTOTUNE)
     data = data.batch(128)
     print('Preprocessing done. Saving output files...')
-    data.save(pp_data_file_output)
+    data.save(pp_dataset_file_output)
 
 parser = argparse.ArgumentParser(description="Preprocess images for MobileNetV2")
-parser.add_argument("--data_file_input", type=str, 
+parser.add_argument("--dataset-file-input", type=str, 
                     help="Directory for input file. Should be tensorflow dataset format.")
-parser.add_argument("--pp_data_file_output", type=str,
+parser.add_argument("--pp_dataset-file-output", type=str,
                     help="Directory for preprocessed dataset output file")
 args = parser.parse_args()
     
-preprocess_component(args.data_file_input, 
-                     args.pp_data_file_output)
+preprocess_component(args.dataset_file_input, 
+                     args.pp_dataset_file_output)
